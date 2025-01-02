4878d150421dad3e0534174adbeecd6354389db3
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2025-01-02 11:18:08 +0100

    BoxCreator för griden
    Har skapat de elementen som senare ska kunna skapa hur många boxar man vill ha i griden.

--- common.js
+++ common.js
@@ -5,3 +5,20 @@ let homeLink = document.createElement("a");
 homeLink.textContent = "Home";
 homeLink.href = "../index.html";
 navHome.appendChild(homeLink);
+
+
+
+// skapa header som innehåller först en gemensam för alla sidor boxCreator
+let createElement = document.getElementById("creator");
+
+let creatorText = document.createElement("p");
+creatorText.textContent = "How many numbers in the grid?";
+createElement.appendChild(creatorText)
+
+let creatorInput = document.createElement("input");
+creatorInput.placeholder = 95;
+createElement.appendChild(creatorInput);
+
+let creatorButton = document.createElement("button");
+creatorButton.textContent = "Create";
+createElement.appendChild(creatorButton)
