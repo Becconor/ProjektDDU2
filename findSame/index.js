da5fa2f1cf7dc90faa8543f83f27633585772ec6
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2025-01-07 17:29:14 +0100

    FindSame
    Klar med index.js och index.css

---
+++ findSame/index.js
@@ -0,0 +1,35 @@
+function findSame() {
+    let boxes = document.querySelectorAll(".boxes");
+    let reset = document.getElementById("reset");
+    let text = document.getElementById("text");
+
+    for (let box of boxes) {
+        box.addEventListener("click", function () {
+            let counter = 0;
+
+            for (let sameBox of boxes) {
+                if (box.textContent == sameBox.textContent) {
+                    sameBox.classList.add("clicked");
+                    counter += 1;
+                } else {
+                    sameBox.classList.remove("clicked");
+                    text.textContent = "";
+                }
+
+                text.textContent = `${counter} copies of the number ${box.textContent}`;
+            }
+        });
+
+        reset.addEventListener("click", function () {
+            text.textContent = "Click on a number to find copies";
+
+            for (let sameBox of boxes) {
+                sameBox.classList.remove("clicked");
+                sameBox.textContent = sameBox.textContent;
+            }
+        });
+    };
+
+};
+
+findSame();
