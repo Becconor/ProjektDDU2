03ec68bee8f45a90e503b7d59eca7f5e011a241e
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2025-01-07 17:23:50 +0100

    Sum index.js är klar

---
+++ sum/index.js
@@ -0,0 +1,36 @@
+function sum() {
+    let boxes = document.querySelectorAll(".boxes");
+    let reset = document.getElementById("reset");
+    let sumOfMarked = document.getElementById("text");
+    let sumCounter = 0;
+    let markedCounter = 0;
+
+
+    for (let box of boxes) {
+        sumCounter += Number(box.textContent);
+    }
+
+    let sumOfAll = document.getElementById("sumOfAll");
+    sumOfAll.textContent = sumCounter;
+
+    for (let box of boxes) {
+        box.addEventListener("click", function () {
+            box.classList.add("clicked");
+            markedCounter += Number(box.textContent);
+
+            sumOfMarked.textContent = markedCounter;
+        }, { once: true });
+
+
+        reset.addEventListener("click", function () {
+            markedCounter = 0;
+            sumOfMarked.textContent = "-";
+
+            for (let sameBox of boxes) {
+                sameBox.classList.remove("clicked");
+            }
+        });
+    }
+};
+
+sum();
